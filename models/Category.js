9379e1deee4f4855ca889891668f7448b4bba8b2
unknown <elsie.dh@gmail.com>
2023-12-13 22:24:10 -0800

    added Category model

---
+++ models/Category.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const { truncate } = require('lodash');
+
+class Category extends Model {}
+
+Category.init(
+    {
+        id: {
+            type: DataTypes.INTIGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true ,
+        },
+
+        category_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'category',
+    }
+);
+
+module.exports = Category;
